6a37acafd66a8453af9450b15f6a52285e381c99
james7132 <james.siqi.liu@gmail.com>
2018-02-22 17:31:42 -0800

    Add Nep Games

--- src/js/data/2018-02-22.js
+++ src/js/data/2018-02-22.js
@@ -8,21 +8,43 @@ dataSet[dataSetVersion].options = [
     tooltip: "Check this to restrict to certain series.",
     checked: false,
     sub: [
-      { name: "Embodiment of Scarlet Devil", tooltip: "06 - Koumakan", key: "EoSD" },
+      { name: "Hyperdimension Neptunia", key: "hdn" },
+      { name: "Hyperdimension Neptunia Mk2", key: "mk2" },
+      { name: "Hyperdimension Neptunia Victory", key: "v" },
+      { name: "Megadimension Neputnia VII", key: "vii" },
+      { name: "Hyperdimension Neptunia: Producing Perfection", key: "pp" },
+      { name: "Hyperdevotion Noire: Goddess Black Heart", key: "gbh" },
+      { name: "Hyperdimension Neptunia U: Action Unleashed", key: "au" },
+      { name: "MegaTagmension Blanc + Neptune VS Zombies", key: "zomb" },
+      { name: "Superdimension Neptune vs Sega Hard Girls", key: "sega" },
+      { name: "Cyberdimension Neptunia: 4 Goddesses Online", key: "fgo" },
+      { name: "Nep-Nep Connect: Chaos Chanpuru", key: "nnc" },
+      { name: "Hyperdimension Neptunia Re;birth 1", key: "rb1" },
+      { name: "Hyperdimension Neptunia Re;birth 2: Sisters Generation", key: "rb2" },
+      { name: "Hyperdimension Neptunia Re;birth 3: V Generation", key: "rb3" },
+      { name: "Megadimension Neptunia VIIR", key: "viir" },
     ]
   },
   {
-    name: "Filter by Stage Enemy Appearances",
-    key: "stage",
+    name: "Filter by Character Type",
+    key: "type",
     tooltip: "Check this to restrict to characters that appear in certain stages as enemies.",
     checked: false,
-    sub: [ { name: "Stage 1", key: "st1" }, { name: "Stage 2", key: "st2" }, { name: "Stage 3", key: "st3" }, { name: "Stage 4", key: "st4" }, { name: "Stage 5/Penultimate", tooltip: "Stage 4 in 5-stage games, and Stage 8 in 9-stage games.", key: "st5" }, { name: "Stage 6/Final", key: "st6" }, { name: "Stage EX/Phantasm", key: "ex" }
+    sub: [
+      { name: "CPU", key: "cpu" },
+      { name: "Maker", key: "mkr" },
+      { name: "Monster", key: "msr" },
     ]
   },
   {
-    name: "Remove PC-98 Duplicates",
-    key: "pc98",
-    tooltip: "Check this to remove PC-98 characters with a Windows counterpart."
+    name: "Remove alternate forms",
+    key: "form",
+    tooltip: "Check this to remove alternate form counterparts to characters."
+  },
+  {
+    name: "Remove different dimension duplicates",
+    key: "dim",
+    tooltip: "Check this to remove alternate dimension counterparts to characters."
   },
   {
     name: "Remove Non-Girls",
@@ -36,48 +58,68 @@ dataSet[dataSetVersion].characterData = [
     name: "Neptune",
     img: "c5DqpgX.png",
     opts: {
+      game: [ "hdn", "mk2", "v", "vii", "pp", "gbh", "au", "zomb", "sega", "fgo", "nnc", "rb1", "rb2", "rb3", "viir" ],
+      type: [ "cpu" ]
     }
   },
   {
     name: "Noire",
     img: "tJnkSzK.png",
     opts: {
+      game: [ "hdn", "mk2", "v", "vii", "pp", "gbh", "au", "zomb", "fgo", "nnc", "rb1", "rb2", "rb3", "viir" ],
+      type: [ "cpu" ]
     }
   },
   {
     name: "Vert",
     img: "0YT7QlS.png",
     opts: {
+      game: [ "hdn", "mk2", "v", "vii", "pp", "gbh", "au", "zomb", "fgo", "nnc", "rb1", "rb2", "rb3", "viir" ],
+      type: [ "cpu" ]
     }
   },
   {
     name: "Blanc",
     img: "NWlZud3.png",
     opts: {
+      game: [ "hdn", "mk2", "v", "vii", "pp", "gbh", "au", "zomb", "fgo", "nnc", "rb1", "rb2", "rb3", "viir" ],
+      type: [ "cpu" ]
     }
   },
   {
     name: "Purple Heart",
     img: "qdveFSy.png",
     opts: {
+      game: [ "hdn", "mk2", "v", "vii", "pp", "gbh", "au", "zomb", "sega", "fgo", "nnc", "rb1", "rb2", "rb3", "viir" ],
+      type: [ "cpu" ],
+      form: true,
     }
   },
   {
     name: "Black Heart",
     img: "ptGp0x4.png",
     opts: {
+      game: [ "hdn", "mk2", "v", "vii", "pp", "gbh", "au", "zomb", "fgo", "nnc", "rb1", "rb2", "rb3", "viir" ],
+      type: [ "cpu" ],
+      form: true,
     }
   },
   {
     name: "Green Heart",
     img: "vBKdDm4.png",
     opts: {
+      game: [ "hdn", "mk2", "v", "vii", "pp", "gbh", "au", "zomb", "fgo", "nnc", "rb1", "rb2", "rb3", "viir" ],
+      type: [ "cpu" ],
+      form: true,
     }
   },
   {
     name: "White Heart",
     img: "A7ZnuHo.png",
     opts: {
+      game: [ "hdn", "mk2", "v", "vii", "pp", "gbh", "au", "zomb", "fgo", "nnc", "rb1", "rb2", "rb3", "viir" ],
+      type: [ "cpu" ],
+      form: true,
     }
   },
 ];
